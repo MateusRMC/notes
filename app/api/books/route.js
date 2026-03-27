5cf0f4e3f381072e989c2141aed5b88b9ba9d248
Mateus Rocha <mateus@Mattbook.local>
2026-03-27 17:35:37 -0400

    book flow is done

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -14,3 +14,22 @@ export async function GET() {
 
   return NextResponse.json(data);
 }
+
+export async function POST(req) {
+  const body = await req.json();
+
+  const { data, error } = await supabase
+    .schema("notes")
+    .from("books")
+    .insert([
+      {
+        title: body.title,
+      },
+    ]);
+
+  if (error) {
+    return NextResponse.json({ error: error.message });
+  }
+
+  return NextResponse.json(data);
+}
--- app/page.js
+++ app/page.js
@@ -5,12 +5,28 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [books, setBooks] = useState([]);
+  const [newBook, setNewBook] = useState("");
 
   async function getBooks() {
     const req = await fetch("/api/books/");
     const res = await req.json();
 
     setBooks(res);
+  }
+
+  async function postBooks(e) {
+    e.preventDefault();
+
+    const req = await fetch("/api/books/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: newBook }),
+    });
+
+    setNewBook("");
+    getBooks();
   }
 
   useEffect(() => {
@@ -23,9 +39,21 @@ export default function Home() {
       <div className="showBooks">
         {books.map((book) => (
           <Link key={book.id} href={`/book?id=${book.id}`}>
-            <p key={book.id}>{book.title}</p>
+            <p>{book.title}</p>
           </Link>
         ))}
+      </div>
+      <div className="newBook">
+        <form onSubmit={postBooks}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Book title"
+            onChange={(e) => setNewBook(e.target.value)}
+            value={newBook}
+          />
+          <input type="submit" value="Create new book" />
+        </form>
       </div>
     </div>
   );
