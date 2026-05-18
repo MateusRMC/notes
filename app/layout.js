f413d0333230262f7bccceb5fa71662b1979947e
Mateus Rocha <mateus@Mattbook.local>
2026-05-18 04:42:50 -0400

    splash art for IOS in pwa2

--- app/layout.js
+++ app/layout.js
@@ -6,20 +6,33 @@ export const metadata = {
   title: "Simple Notes - Write and nothing more.",
   description: "Write and nothing more.",
   applicationName: "Simple Notes",
+
   icons: {
     icon: "/icons/favicon-196.png",
     apple: "/icons/apple-icon-180.png",
   },
+
   appleWebApp: {
     capable: true,
     title: "Simple Notes",
     statusBarStyle: "default",
     startupImage: iosStartupImages,
   },
+
+  other: {
+    "mobile-web-app-capable": "yes",
+    "apple-mobile-web-app-capable": "yes",
+    "apple-mobile-web-app-title": "Simple Notes",
+    "apple-mobile-web-app-status-bar-style": "default",
+  },
 };
 
 export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
   themeColor: "#ffffff",
+  colorScheme: "light",
 };
 
 export default function RootLayout({ children }) {
